ef8c19096b91b95fdf036b7a3703e8a58535ec2b
Anamika Sawhney <anamika.sawhney22@gmail.com>
2022-04-19 15:20:52 +0530

    MVC Demo:

---
+++ WebApplication7/Controllers/FirstController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication7.Controllers
+{
+    public class FirstController : Controller
+    {
+        // GET: First
+        public ActionResult Index()
+        {
+            if(TempData["name"]!=null)
+            {
+                ViewBag.name = TempData["name"];
+            }
+            ViewBag.date = DateTime.Now.ToString();
+            return View();
+        }
+        public ActionResult Index1()
+        {
+            ViewData["data"] = DateTime.Now.ToString();
+            return View();
+        }
+
+        public ActionResult Index3()
+        {
+            TempData["name"] = "Ajay Kumar";
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ WebApplication7/Controllers/StudentController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication7.Models;
+namespace WebApplication7.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            List<Student> list = new List<Student>()
+        {
+            new Student() { Id=1, Name="Ajay", Batch="B001", Marks=90},
+
+            new Student() { Id=2, Name="Ajay", Batch="B001", Marks=90},
+
+            new Student() { Id=3, Name="Ajay", Batch="B001", Marks=90},
+
+            new Student() { Id=4, Name="Ajay", Batch="B001", Marks=90},
+
+            new Student() { Id=5, Name="Ajay", Batch="B001", Marks=90},
+
+            new Student() { Id=6, Name="Ajay", Batch="B001", Marks=90}
+        };
+            ViewBag.list = list;
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            Student student = new Student()
+            { Id = 100, Name = "Deepak", Batch = "B001", Marks = 90 };
+            ViewBag.student = student;
+            return View();
+        }
+
+        public ActionResult Details1()
+        {
+            Student student = new Student()
+            { Id = 100, Name = "Deepak", Batch = "B001", Marks = 90 };
+            return View(student);
+        }
+        public ActionResult Index1()
+        {
+            List<Student> list = new List<Student>()
+        {
+            new Student() { Id=1, Name="Ajay", Batch="B001", Marks=90},
+
+            new Student() { Id=2, Name="Ajay", Batch="B001", Marks=90},
+
+            new Student() { Id=3, Name="Ajay", Batch="B001", Marks=90},
+
+            new Student() { Id=4, Name="Ajay", Batch="B001", Marks=90},
+
+            new Student() { Id=5, Name="Ajay", Batch="B001", Marks=90},
+
+            new Student() { Id=6, Name="Ajay", Batch="B001", Marks=90}
+        };
+            
+            return View(list);
+        }
+
+    }
+}
